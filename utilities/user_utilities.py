7b6722813a07abbe7b8150cae583c9d816914e24
FabioRimondi <techmuffin@outlook.it>
2021-07-17 00:56:14 +0200

    Fixed some mongodb Query from the user_utilities

--- utilities/user_utilities.py
+++ utilities/user_utilities.py
@@ -5,7 +5,10 @@ class user_utilities:
     def verify_if_user_exist(user_id):
         user = users.find({ "user_id":user_id})
 
-        if user == []:
+        user = [x for x in user]
+
+        # If it doesn't find any user, the array will be len -> 0
+        if len(user) == 0:
             return False
         else:
             return True
@@ -18,7 +21,7 @@ class user_utilities:
 
     @staticmethod
     def user_language(user_id):
-        user = users.find({ "user_id":user_id})
+        user = users.find({"user_id":user_id})
         return user[0]['language']
 
     @staticmethod
@@ -28,15 +31,18 @@ class user_utilities:
 
     @staticmethod
     def get_remember_list(user_id):
-        remembers = remembers.find({"user_id":user_id})
-        return remembers
+        remembers_list = [remember for remember in remembers.find({"user_id":user_id})]
+
+        return remembers_list
     
     @staticmethod
     def change_user_language(user_id, language):
-        user = { "user_id":user_id}
-        new_language = {"language" : language}
+
+        user_query = { "user_id":user_id}
+        new_user = users.find({ "user_id":user_id})[0]
+        new_user['language'] = language
         
-        users.replace_one(user, new_language )
+        users.replace_one(user_query, new_user )
         return
 
 
